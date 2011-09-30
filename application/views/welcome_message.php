019cf2f7ad14c1ac24657aabdfa0c47dff5261f2
Roberto Urita Jimenez <robertuj@gmail.com>
2011-09-30 03:39:52 -0700

    edit test

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -70,10 +70,10 @@
 	<h1>Welcome to CodeIgniter!</h1>
 
 	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+		<p>Edit</p>
 
 		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
+		<code>application/views/welcome_message.php  -- by @RobertUJ</code>
 
 		<p>The corresponding controller for this page is found at:</p>
 		<code>application/controllers/welcome.php</code>
